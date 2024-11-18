e25be51a7d8b6bbbacc3875f6ef33cccb69e4d1d
Muhammad-Dzaki-Arrahman <kiarrahman05@gmail.com>
2024-11-19 06:22:14 +0700

    Added Work Page
    Add Slider to Display Image, Create Description for the image

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 
-import { Swiper, SwiperSlide, SwiperSlider } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
@@ -16,6 +16,9 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+
+// Components
+import WorkSliderButton from "@/components/WorkSliderButton";
 
 const projects = [
   {
@@ -32,7 +35,7 @@ const projects = [
       { name: "MySql" },
       { name: "Xampp" },
     ],
-    image: "'assets/images/works/project1.png'",
+    image: "/assets/images/works/project1.jpg",
     github: "",
   },
   {
@@ -49,7 +52,7 @@ const projects = [
       { name: "MySql" },
       { name: "Xampp" },
     ],
-    image: "'assets/images/works/project2.png'",
+    image: "/assets/images/works/project2.png",
     github: "",
   },
   {
@@ -66,7 +69,7 @@ const projects = [
       { name: "NodeJS" },
       { name: "NPM" },
     ],
-    image: "'assets/images/works/project3.png'",
+    image: "/assets/images/works/project3.png",
     github: "",
   },
   {
@@ -85,7 +88,7 @@ const projects = [
       { name: "NPM" },
       { name: "ShadCN" },
     ],
-    image: "'assets/images/works/project4.png'",
+    image: "/assets/images/works/project4.png",
     github: "",
   },
 ];
@@ -101,11 +104,14 @@ const Work = () => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-8">
+        <div className="flex flex-col xl:flex-row xl:gap-8 ">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-8 h-[50%]">
               {/* Outline Num */}
@@ -119,7 +125,7 @@ const Work = () => {
               {/* Project Description */}
               <p className="text-white/60">{project.description}</p>
               {/* Stack */}
-              <ul className="flex gap-4">
+              <ul className="flex gap-4 flex-wrap">
                 {project.stack.map((item, index) => {
                   return (
                     <li key={index} className="text-xl text-accent">
@@ -171,8 +177,27 @@ const Work = () => {
               onSlideChange={handleSlideChange}
             >
               {projects.map((project, index) => {
-                return <SwiperSlide key={index}>Slide</SwiperSlide>;
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] flex justify-center items-center relative group bg-purple-50">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-purple-50/20 z-10"></div>
+                      {/* image */}
+                      <Image
+                        src={project.image}
+                        fill
+                        className="w-object-cover"
+                        alt=""
+                      />
+                    </div>
+                  </SwiperSlide>
+                );
               })}
+              {/* Swiber Buttons */}
+              <WorkSliderButton
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 justify-between w-full z-20 xl:w-max xl:justify-none"
+                buttonStyles="bg-accent hover:bg-accent-hover text-primary text-2xl w-11 h-11 flex justify-center items-center transition-all"
+              />
             </Swiper>
           </div>
         </div>
